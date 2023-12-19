8c386d31370dfca35b860c062484546877c4b39f
Fer <F_er1013@hotmail.com>
2023-12-18 21:30:28 -0300

    cors1

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const routes = require('./routes/routes.js');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+
 
 app.use(cors());
 app.use(bodyParser.json()); // Analiza JSON
